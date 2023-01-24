d59b6e35e0990d5cb0f07800f8a47ede1101f70b
koen-dev-code <koen8522@gmail.com>
2023-01-24 09:49:06 -0600

    h

--- src/BallPanel.java
+++ src/BallPanel.java
@@ -33,7 +33,7 @@ public class BallPanel extends JPanel
 
             }
         });
-        for(int i = 0; i <=50 ; i++)
+        for(int i = 0; i <=20 ; i++)
         {
             balls.add(new BallClass());
         }
